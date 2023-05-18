bf5bfd7dbd3077ca639da4f52d80207903a67710
rushanni <rushvm18@gmail.com>
2023-05-18 10:22:58 +0200

    proyecto12

---
+++ m3-angula-desarrollo/5.enrrutado/proyecto12/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
